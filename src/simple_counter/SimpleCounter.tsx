f31865db2e541d44f6f6e0a97fc103bd6918c9c4
hrsk <hrsk.yegor@gmail.com>
2024-03-31 16:55:36 +0300

    useState, increment, decrement value, disabled

--- src/simple_counter/SimpleCounter.tsx
+++ src/simple_counter/SimpleCounter.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import style from './SimpleCounter.module.css'
 
 export const SimpleCounter = () => {
+
+    const [value, setValue] = useState<number>(0)
+
+    const increment = () => {
+        setValue(value + 1)
+    }
+
+    const decrement = () => {
+        setValue(value - 1)
+    }
+
     return (
         <div className={style.container}>
             <div className={style.title}>COUNTER</div>
             <div className={style.content}>
-                <div className={style.counterDisplay}>0</div>
+                <div className={style.counterDisplay}>{value}</div>
                 <div className={style.blockButtons}>
-                    <button className={style.button}>INC</button>
-                    <button className={style.button}>DEC</button>
+                    <button className={style.button} onClick={increment}>INC</button>
+                    <button className={style.button} onClick={decrement} disabled={value === 0}>DEC</button>
                 </div>
             </div>
         </div>
