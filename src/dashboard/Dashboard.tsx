b9caed4c92a04e9638ae257118e6eaa019f03b30
hrsk <hrsk.yegor@gmail.com>
2024-04-03 19:11:30 +0300

    dashboard component layout and styles

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,11 +1,17 @@
 import { NavLink } from "react-router-dom";
+import style from './Dashboard.module.css'
 
 export const Dashboard = () => {
     return (
-        <div>
-            <div>
-                <NavLink to={'/counter-with-settings'}>COUNTER WITH SETTINGS</NavLink>
-                <NavLink to={'/simple-counter'}>SIMPLE COUNTER</NavLink>
+        <div className={style.container}>
+            <div className={style.title}>Dashboard</div>
+            <div className={style.blockButtons}>
+                <button className={style.button}>
+                    <NavLink className={style.link} to={'/counter-with-settings'}>COUNTER WITH SETTINGS</NavLink>
+                </button>
+                <button className={style.button}>
+                    <NavLink className={style.link} to={'/simple-counter'}>SIMPLE COUNTER</NavLink>
+                </button>
             </div>
         </div>
     );
