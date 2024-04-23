0a30bd8f42f83afac26283cadbf510856890ca99
hrsk <hrsk.yegor@gmail.com>
2024-04-23 19:39:20 +0300

    add styled components, styling dashboard component with use styled components

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,18 +1,117 @@
-import { NavLink } from "react-router-dom";
-import style from './Dashboard.module.css'
+import { NavLink as BaseNavLink } from "react-router-dom";
+import styled from "styled-components";
 
 export const Dashboard = () => {
     return (
-        <div className={style.container}>
-            <div className={style.title}>Dashboard</div>
-            <div className={style.blockButtons}>
-                <button className={style.button}>
-                    <NavLink className={style.link} to={'/counter-with-settings'}>COUNTER WITH SETTINGS</NavLink>
-                </button>
-                <button className={style.button}>
-                    <NavLink className={style.link} to={'/simple-counter'}>SIMPLE COUNTER</NavLink>
-                </button>
-            </div>
-        </div>
+        <Wrapper>
+            <Title>Dashboard</Title>
+            <LinksWrapper>
+                <NavLink to={'/counter-with-settings'}>counter with settings</NavLink>
+                <NavLink to={'/simple-counter'}>simple counter</NavLink>
+            </LinksWrapper>
+        </Wrapper>
     );
 };
+
+const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 300px;
+    height: 350px;
+    background: '';
+    border-radius: 15px;
+
+    box-shadow: 0px 4px 20px 5px #0000003c;
+`
+const Title = styled.h3<{ $textColor?: string }>`
+    position: sticky;
+    width: 280px;
+    height: 40px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 10px;
+    margin-bottom: 20px;
+    background: linear-gradient(89.94deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%);
+
+    font: Inter;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 16px;
+    text-align: left;
+    color: ${props => props.$textColor || "#FFFFFF"};
+`
+const NavLink = styled(BaseNavLink) <{ $textColor?: string }>`
+    position: relative;
+    width: 200px;
+    height: 60px;
+
+    border-radius: 34px;
+    background: #fff;
+
+    align-content: center;
+    text-align: center;
+    text-decoration: none;
+    text-transform: uppercase;
+
+    color: ${props => props.$textColor || "#7785ff"};
+    font: Inter;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 14px;
+
+    &:active {
+        color: ${props => props.$textColor || "#7785ff"};
+
+        text-decoration: none;
+    }
+
+    &:visited {
+        color: ${props => props.$textColor || "#7785ff"};
+
+        text-decoration: none;
+    }
+    &:hover {
+        color: ${props => props.$textColor || "#b279ff"};
+    }
+
+    &::before {
+    content: "";
+    position: absolute;
+    top: -2px;
+    bottom: -2px;
+    left: -2px;
+    right: -2px;
+    background: linear-gradient(89.94deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%);
+    border-radius: 34px;
+    z-index: -1;
+    }
+    
+    &:hover::before {
+    content: "";
+    position: absolute;
+    top: -2px;
+    bottom: -2px;
+    left: -2px;
+    right: -2px;
+    background: linear-gradient(89.94deg, #ff81ed 0%, #b279ff 52.6%, #7785ff 73.96%, #51b9ff 100%);
+    border-radius: 34px;
+    z-index: -1;    
+}   
+`;
+
+const LinksWrapper = styled.div`
+    margin-top: 40px;
+
+    width: 280px;
+    height: 70px;
+    border-radius: 10px;
+    gap: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
