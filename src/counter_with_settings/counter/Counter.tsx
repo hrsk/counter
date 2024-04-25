e45a563b1d68d6572af75f6f2cd2b1ffe7202627
hrsk <hrsk.yegor@gmail.com>
2024-04-25 12:08:34 +0300

    styling counter component with using styled components

--- src/counter_with_settings/counter/Counter.tsx
+++ src/counter_with_settings/counter/Counter.tsx
@@ -1,15 +1,16 @@
 import { useDispatch } from 'react-redux'
 import { setCounterValue } from '../../reducers/counter-with-settings-reducer'
-import style from './Counter.module.css'
 import { useSelector } from 'react-redux'
 import { AppStateType } from '../../store/store'
 import { useNavigate } from 'react-router-dom'
+import { styled } from 'styled-components'
 
 type PropsType = {
     counterValue: number | undefined
     startValue: number
     maxValue: number
 }
+
 export const Counter = (props: PropsType) => {
 
     const dispatch = useDispatch()
@@ -39,29 +40,120 @@ export const Counter = (props: PropsType) => {
     const counterValueIsMax = props.counterValue === props.maxValue
 
     return (
-        <div className={style.container}>
-            <div className={style.title}>Counter</div>
-            <div className={style.content}>
-                <div style={error ? { color: 'crimson' } : {}}
-                    className={style.counterDisplay}>
-                    {
-                        error
-                            ? <span>{error}</span>
-                            : <span style={counterValueIsMax ? { color: 'crimson', fontWeight: 'bold' } : {} && { fontWeight: 'bold' }} >{props.counterValue}</span>
-                    }
-                </div>
-                <div className={style.blockButtons}>
-                    <button style={incrementDisabled || incorrectValue ? { borderColor: 'crimson' } : {}}
-                        className={style.button}
-                        onClick={increment}
-                        disabled={incrementDisabled}>Increment</button>
-                    <button style={resetDisabled ? { borderColor: 'crimson' } : {}}
-                        className={style.button}
-                        onClick={reset}
-                        disabled={resetDisabled}>Reset</button>
-                    <button className={style.button} onClick={onClickSettingsHandler}>Settings</button>
-                </div>
-            </div>
-        </div>
+        <Wrapper>
+            <Title>Counter</Title>
+            <DisplayCounterValue>
+                {
+                    error
+                        ? <Text $textColor={error ? 'crimson' : ''}>{error}</Text>
+                        : <Text $textColor={counterValueIsMax ? '#543fbe ' : ''} $fontSize={counterValueIsMax ? '48px' : ''}>{props.counterValue}</Text>
+                }
+            </DisplayCounterValue>
+            <ButtonsWrapper>
+                <Button
+                    onClick={increment}
+                    disabled={incrementDisabled || incorrectValue}
+                >Increment</Button>
+                <Button
+                    onClick={reset}
+                    disabled={resetDisabled}
+                >Reset</Button>
+                <Button onClick={onClickSettingsHandler}>Settings</Button>
+            </ButtonsWrapper>
+        </Wrapper>
     )
 }
+
+
+const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 300px;
+    height: 350px;
+    border-radius: 34px;
+
+    box-shadow: 0px 4px 20px 5px #0000003c;
+`
+
+const Title = styled.h3<{ $textColor?: string }>`
+    position: sticky;
+    width: 280px;
+    height: 40px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 34px;
+    background: linear-gradient(89.94deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%);
+
+    font: Inter;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 16px;
+    text-align: left;
+    color: ${props => props.$textColor || "#FFFFFF"};
+`
+
+const ButtonsWrapper = styled.div`
+    width: 280px;
+    height: 70px;
+    border-radius: 10px;
+    gap: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const DisplayCounterValue = styled.div<{ $textColor?: string }>`
+    width: 280px;
+    height: 120px;
+    border-radius: 34px;
+    background: 
+        linear-gradient(#fff, #fff) padding-box,
+        linear-gradient(60deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%) border-box;
+        border: 2px solid transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Button = styled.button<{ $textColor?: string }>`
+    width: 96px;
+    height: 32px;
+
+    background: 
+        linear-gradient(#fff, #fff) padding-box,
+        linear-gradient(60deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%) border-box;
+    border: 2px solid transparent;
+    border-radius: 34px;
+
+    color: ${props => props.$textColor || "#7785ff"};
+    font: Inter;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 14px;
+
+    &:hover {
+        color: ${props => props.$textColor || "#b279ff"};
+        background: 
+        linear-gradient(#fff, #fff) padding-box,
+        linear-gradient(60deg, #ff81ed 0%, #b279ff 52.6%, #7785ff 73.96%, #51b9ff 100%) border-box;
+    border: 2px solid transparent;
+    border-radius: 34px;
+    }
+
+    &:disabled {    
+    color: #cccaca;
+    border: 2px solid #cccaca;
+    border-radius: 34px;
+    }
+    &:disabled:hover {
+        color: #cccaca
+    }
+`;
+
+const Text = styled.span<{ $textColor?: string, $fontSize?: string }>`
+color: ${props => props.$textColor || '#7785ff'};
+font-size: ${props => props.$fontSize || '16px'};
+`
