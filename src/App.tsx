c08b744ff05faaef81fcd57e601023a5587f42e9
hrsk <hrsk.yegor@gmail.com>
2024-04-27 18:24:31 +0300

    styling simple counter with use styled components, moving useEffects to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,62 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { CounterWithSettings } from './counter_with_settings/CounterWithSettings';
 import { Settings } from './counter_with_settings/settings/Settings';
 import { Dashboard } from './dashboard/Dashboard';
-import { InitialStateType } from './reducers/counter-with-settings-reducer';
+import { InitialStateType, setMaxValue, setStartValue } from './reducers/counter-with-settings-reducer';
 import { SimpleCounter } from './simple_counter/SimpleCounter';
 import { AppStateType } from './store/store';
+import { useEffect } from 'react';
+import { setCounterError } from './reducers/counter-errors-reducer';
+import { setCounterValue } from './reducers/simple-counter-reducer';
 
 export const App = () => {
 
   const values = useSelector<AppStateType, InitialStateType>(state => state.counterWithSettings)
   const counterValue = useSelector<AppStateType, number>((state) => state.simpleCounter.value)
+
+  const dispatch = useDispatch()
+
+  // useEffect on first load page: check localstorage on presence of values and set
+
+  useEffect(() => {
+    dispatch(setCounterValue(0))
+  }, [])
+
+  const maxValueFromLocalStorage = () => {
+    const valueAsString = localStorage.getItem("maxValue");
+    if (valueAsString) {
+      return JSON.parse(valueAsString)
+    } else return values.maxValue
+  }
+
+  const startValueFromLocalStorage = () => {
+    const valueAsString = localStorage.getItem("startValue");
+    if (valueAsString) {
+      return JSON.parse(valueAsString)
+    } else return values.startValue
+  }
+
+  useEffect(() => {
+    dispatch(setMaxValue(maxValueFromLocalStorage()))
+    dispatch(setStartValue(startValueFromLocalStorage()))
+  }, [])
+
+  // useEffect errors 
+
+  useEffect(() => {
+    if (values.counterValue === undefined) {
+      dispatch(setCounterError([`Enter values and press 'Save'!`]));
+    }
+    if (values.maxValue < values.startValue || values.startValue < 0 || values.maxValue < 0 || values.startValue === values.maxValue) {
+      dispatch(setCounterError(['Input values is incorrect!']))
+    }
+    if (values.counterValue === values.startValue) {
+      dispatch(setCounterError([]));
+    }
+  }, [values.startValue, values.maxValue, values.counterValue]);
+
 
   return (
     <div className="App">
--- src/simple_counter/SimpleCounter.tsx
+++ src/simple_counter/SimpleCounter.tsx
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { setCounterValue } from '../reducers/simple-counter-reducer'
-import style from './SimpleCounter.module.css'
+import { styled } from 'styled-components'
 
 type PropsType = {
     counterValue: number
@@ -27,18 +27,109 @@ export const SimpleCounter = (props: PropsType) => {
     const disabled = props.counterValue === 0
 
     return (
-        <div className={style.container}>
-            <div className={style.title}>SIMPLE COUNTER</div>
-            <div className={style.content}>
-                <div className={style.counterDisplay}>{props.counterValue}</div>
-                <div className={style.blockButtons}>
-                    <button className={style.button} onClick={incrementHandler}>INC</button>
-                    <button style={disabled ? { borderColor: 'crimson' } : {}}
-                        className={style.button}
-                        onClick={decrementHandler}
-                        disabled={disabled}>DEC</button>
-                </div>
-            </div>
-        </div>
+        <Wrapper>
+            <Title>Simple Counter</Title>
+            <DisplayCounterValue>
+                <Text>{props.counterValue}</Text>
+            </DisplayCounterValue>
+            <ButtonsWrapper>
+                <Button onClick={incrementHandler}>INC</Button>
+                <Button onClick={decrementHandler}
+                    disabled={disabled}>DEC</Button>
+            </ButtonsWrapper>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 300px;
+    height: 350px;
+    border-radius: 34px;
+
+    box-shadow: 0px 4px 20px 5px #0000003c;
+`
+
+const Title = styled.h3<{ $textColor?: string }>`
+    position: sticky;
+    width: 280px;
+    height: 40px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 34px;
+    background: linear-gradient(89.94deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%);
+
+    font: Inter;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 16px;
+    text-align: left;
+    color: ${props => props.$textColor || "#FFFFFF"};
+`
+
+const ButtonsWrapper = styled.div`
+    width: 280px;
+    height: 70px;
+    border-radius: 10px;
+    gap: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const DisplayCounterValue = styled.div<{ $textColor?: string }>`
+    width: 280px;
+    height: 120px;
+    border-radius: 34px;
+    background: 
+        linear-gradient(#fff, #fff) padding-box,
+        linear-gradient(60deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%) border-box;
+        border: 2px solid transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Button = styled.button<{ $textColor?: string }>`
+    width: 96px;
+    height: 32px;
+
+    background: 
+        linear-gradient(#fff, #fff) padding-box,
+        linear-gradient(60deg, #51b9ff 0%, #7785ff 52.6%, #b279ff 73.96%, #ff81ed 100%) border-box;
+    border: 2px solid transparent;
+    border-radius: 34px;
+
+    color: ${props => props.$textColor || "#7785ff"};
+    font: Inter;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 14px;
+
+    &:hover {
+        color: ${props => props.$textColor || "#b279ff"};
+        background: 
+        linear-gradient(#fff, #fff) padding-box,
+        linear-gradient(60deg, #ff81ed 0%, #b279ff 52.6%, #7785ff 73.96%, #51b9ff 100%) border-box;
+    border: 2px solid transparent;
+    border-radius: 34px;
+    }
+
+    &:disabled {    
+    color: #cccaca;
+    border: 2px solid #cccaca;
+    border-radius: 34px;
+    }
+    &:disabled:hover {
+        color: #cccaca
+    }
+`
+
+const Text = styled.span<{ $textColor?: string, $fontSize?: string }>`
+color: ${props => props.$textColor || '#7785ff'};
+font-size: ${props => props.$fontSize || '16px'};
+`
